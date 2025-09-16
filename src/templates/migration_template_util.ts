e5f7ca48cfb746968c2f676ea17543511683168b
David Matt-Ojo <ojooh@Davids-MacBook-Air.local>
2025-09-16 14:31:57 +0100

    updated migration diff rendering

--- src/templates/migration_template_util.ts
+++ src/templates/migration_template_util.ts
@@ -21,6 +21,8 @@ export default ${class_name}InitialMigration;
 
 export function updateMigrationCodeContent(app_id: string, class_name: string, class_snake_case: string, migration_diff: SchemaSnapshotDifferenceInterface): string {
 
+    const migration_diff_str = JSON.stringify(migration_diff, null, 2);
+    
 return `
 import { ${class_name} } from "@/database_components/schemas/${class_snake_case.toLowerCase()}";
 import BaseMigration from "@/database_components/migrations/base/base_update_migration";
@@ -28,7 +30,7 @@ import BaseMigration from "@/database_components/migrations/base/base_update_mig
 class ${class_name}UpdateMigration extends BaseMigration {
     constructor() {
         super("${class_snake_case.toLowerCase()}_update_migration", ${class_name} );
-        this.migration_diff         = ${migration_diff};
+        this.migration_diff         = ${migration_diff_str};
     }
 }
 
