d496ebf2b38781e09cce220feab18ec535f28d38
David Matt-Ojo <ojooh@Davids-MacBook-Air.local>
2025-09-16 18:42:15 +0100

    fixed seeder template bug

--- src/templates/seeder_template_util.ts
+++ src/templates/seeder_template_util.ts
@@ -3,13 +3,12 @@ export function seederCodeContent(app_id: string, class_name: string, class_snak
     return `
 
 import { ${class_name}  } from "@/database_components/schemas/${class_snake_case.toLowerCase()}";
-import BaseSeeder from "@/database_components/seeders/base_seeder";
+import BaseSeeder from "@/database_components/seeders/base/base_seeder";
 import { SchemaDefinitionInterface } from "@/types/schema_type";
 
 class ${class_name}Seeder extends BaseSeeder {
     constructor(schema: SchemaDefinitionInterface) {
         super("${class_snake_case.toLowerCase()}_seeder", ${class_name} );
-        super("user_seeder", schema);
     }
 
     // Override to provide seed data
